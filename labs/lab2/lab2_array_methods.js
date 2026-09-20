60ac1dfcba1d6150310c672dc6b3855dae7f56ce
JoshuaZaakir <Joshua.Zaakir@georgebrown.ca>
2026-09-20 18:42:02 -0400

    Complete COMP3123 lab 2

---
+++ labs/lab2/lab2_array_methods.js
@@ -0,0 +1,20 @@
+/*
+Purpose:
+
+Array Methods:
+forEach((currentElement) => {})
+map((currentElement) => {})    
+reduce((currentElement) => {})
+filter((currentElement) => {})
+search((currentElement) => {})
+*/
+
+const courseList = ["COMP3123", "COMP3122", "COMP1238", "FASH1011"]
+courseList.forEach((course) => {
+    console.log(course + "!")
+})
+
+const filteredCourses = courseList.filter((currentElement) => {
+    return currentElement[0] === "C"
+})
+console.log(filteredCourses)
